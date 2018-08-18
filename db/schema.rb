57711aa2ec027816e39d209dac8fbff7e430c7af
gbagaev <gbagaev@gmail.com>
2018-08-17 19:22:23 -0700

    Add model user and table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818021004) do
+ActiveRecord::Schema.define(version: 20180818022014) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20180818021004) do
     t.string "title"
     t.integer "level"
     t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
