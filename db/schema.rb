7ca68f1287f85b08a4070c2db47de94c34574659
gbagaev <gbagaev@gmail.com>
2018-08-17 19:26:12 -0700

    Add model category and table categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818022014) do
+ActiveRecord::Schema.define(version: 20180818022344) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
     t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
