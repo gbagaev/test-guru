6e3821043f23a66664f8c0da50ff971163e8b0a8
gbagaev <gbagaev@gmail.com>
2018-08-19 08:40:42 -0700

    Add users name null constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180819153530) do
+ActiveRecord::Schema.define(version: 20180819153903) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20180819153530) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
