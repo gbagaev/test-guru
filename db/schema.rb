bb2980b016f8f2697202360e9c4cd56a410be274
gbagaev <gbagaev@gmail.com>
2018-08-19 08:38:10 -0700

    Add answers body null constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818031429) do
+ActiveRecord::Schema.define(version: 20180819153530) do
 
   create_table "answers", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
